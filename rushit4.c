2a5a7f25eca1d23a24d88fdc0736376a9fab2aa4
RUSHIT305 <134259276+RUSHIT305@users.noreply.github.com>
2025-07-29 09:26:05 +0530

    Add files via upload

---
+++ rushit4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<float.h>
+#include<limits.h>
+void main()
+{
+    printf("size of int : %zu",size of (int) );
+    printf("size of long int : %zu",size of (long));
+    printf("size of char : %zu", size of (char));
+    printf("size of float : %zu", size of (float));
+    printf("size of ")
+}

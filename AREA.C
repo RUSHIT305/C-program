bd31d11008532c2a9825d0eb4b466e6b4724f5e8
RUSHIT305 <134259276+RUSHIT305@users.noreply.github.com>
2025-07-29 10:01:47 +0530

    Add files via upload

---
+++ AREA.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float l,b,a,p;
+	clrscr();
+	l=70;
+	b=90;
+	a = l*b;
+	p = 2* (l+b);
+	printf("area= %f",a);
+	printf("perimeter= %f",p);
+	getch();
+}
